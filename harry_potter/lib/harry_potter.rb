8456db72c3c59b70da3178392e8368431e919e23
Iain Kinnish <iainkinnish@Iains-MacBook-Pro.local>
2017-09-27 10:40:46 +0100

    Tweaks + implement discount calculation

--- harry_potter/lib/harry_potter.rb
+++ harry_potter/lib/harry_potter.rb
@@ -1,7 +1,18 @@
-class HarryPotter
+class Shop
 
-  def buy(book)
-    8
+  DISCOUNT_HASH = { 2 => 5, 3 => 10, 4 => 20, 5 => 25 }
+
+  def initialize
+    @basket = []
+  end
+
+  def buy(books)
+    @basket = books
+    8 * (@basket.sum)
+  end
+
+  def discount(number)
+    DISCOUNT_HASH[number]
   end
 
 end
