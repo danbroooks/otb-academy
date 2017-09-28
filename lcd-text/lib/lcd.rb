3a55e295acbf9996e39590ea5d571f9d6dad8229
Dan Brooks <dmbrooks@live.co.uk>
2017-09-28 16:18:23 +0100

    Initial implementation using positions

--- lcd-text/lib/lcd.rb
+++ lcd-text/lib/lcd.rb
@@ -14,10 +14,63 @@ class LCD
   }
 
   def convert(num, scale = 1)
-    #num.to_s.split("").map { |num| NUM_TO_LCD[num.to_i] }
     split_nums = num.split("")
-    (0..2 * scale).map do |i|
-      split_nums.map { |num| NUM_TO_LCD[num][0] }
+    (0..2 * scale).map { |line|
+      split_nums.map { |num|
+        [
+          left(line, num, scale),
+          middle(line, num, scale),
+          right(line, num, scale),
+        ].join
+      }.join
+    }.join("\n")
+  end
+
+  def left(line, num, scale)
+    if line == scale && NUM_TO_LCD[num][3] == 1
+      v(true)
+    elsif line == scale * 2 && NUM_TO_LCD[num][5] == 1
+      v(true)
+    else
+      v(false)
+    end
+  end
+
+  def middle(line, num, scale)
+    if line == 0 && NUM_TO_LCD[num][0] == 1
+      h(true)
+    elsif line == scale && NUM_TO_LCD[num][1] == 1
+      h(true)
+    elsif line == scale * 2 && NUM_TO_LCD[num][2] == 1
+      h(true)
+    else
+      h(false)
+    end
+  end
+
+  def right(line, num, scale)
+    if line == scale && NUM_TO_LCD[num][4] == 1
+      v(true)
+    elsif line == scale * 2 && NUM_TO_LCD[num][6] == 1
+      v(true)
+    else
+      v(false)
+    end
+  end
+
+  def h(on)
+    if on
+      '_'
+    else
+      ' '
+    end
+  end
+
+  def v(on)
+    if on
+      '|'
+    else
+      ' '
     end
   end
 end
