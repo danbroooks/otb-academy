dc15c7e4c6624ecd2f418d178ce7ecbeff8fd81a
Iain Kinnish <iainkinnish@Iains-MacBook-Pro.local>
2017-09-28 09:33:48 +0100

    Attempting part 2 of LCD exercise.

--- lcd-text/spec/lcd_spec.rb
+++ lcd-text/spec/lcd_spec.rb
@@ -5,7 +5,7 @@ describe LCD do
   describe 'convert(num)' do
 
     it "converts a number to a LCD representation (0)" do
-      expect(subject.convert(0)).to eq <<~SQUIGGLY_HEREDOC
+      expect(subject.convert("0")).to eq <<~SQUIGGLY_HEREDOC
       _
      | |
      |_|
@@ -13,7 +13,7 @@ describe LCD do
     end
 
     it "converts 1 to an LCD representation" do
-      expect(subject.convert(1)).to eq <<~SQUIGGLY_HEREDOC
+      expect(subject.convert("1")).to eq <<~SQUIGGLY_HEREDOC
 
         |
         |
@@ -21,7 +21,7 @@ describe LCD do
     end
 
     it "converts 2 to an LCD representation" do
-      expect(subject.convert(2)).to eq <<~SQUIGGLY_HEREDOC
+      expect(subject.convert("2")).to eq <<~SQUIGGLY_HEREDOC
       _
       _|
      |_
@@ -29,7 +29,7 @@ describe LCD do
     end
 
     it "converts 3 to an LCD representation" do
-      expect(subject.convert(3)).to eq <<~SQUIGGLY_HEREDOC
+      expect(subject.convert("3")).to eq <<~SQUIGGLY_HEREDOC
       _
       _|
       _|
@@ -37,7 +37,7 @@ describe LCD do
     end
 
     it "converts 4 to an LCD representation" do
-      expect(subject.convert(4)).to eq <<~SQUIGGLY_HEREDOC
+      expect(subject.convert("4")).to eq <<~SQUIGGLY_HEREDOC
 
         |_|
           |
@@ -45,7 +45,7 @@ describe LCD do
     end
 
     it "converts 5 to an LCD representation" do
-      expect(subject.convert(5)).to eq <<~SQUIGGLY_HEREDOC
+      expect(subject.convert("5")).to eq <<~SQUIGGLY_HEREDOC
       _
      |_
       _|
@@ -53,7 +53,7 @@ describe LCD do
     end
 
     it "converts 6 to an LCD representation" do
-      expect(subject.convert(6)).to eq <<~SQUIGGLY_HEREDOC
+      expect(subject.convert("6")).to eq <<~SQUIGGLY_HEREDOC
       _
      |_
      |_|
@@ -61,7 +61,7 @@ describe LCD do
     end
 
     it "converts 7 to an LCD representation" do
-      expect(subject.convert(7)).to eq <<~SQUIGGLY_HEREDOC
+      expect(subject.convert("7")).to eq <<~SQUIGGLY_HEREDOC
       _
        |
        |
@@ -69,7 +69,7 @@ describe LCD do
     end
 
     it "converts 8 to an LCD representation" do
-      expect(subject.convert(8)).to eq <<~SQUIGGLY_HEREDOC
+      expect(subject.convert("8")).to eq <<~SQUIGGLY_HEREDOC
       _
      |_|
      |_|
@@ -77,7 +77,7 @@ describe LCD do
     end
 
     it "converts 9 to an LCD representation" do
-      expect(subject.convert(9)).to eq <<~SQUIGGLY_HEREDOC
+      expect(subject.convert("9")).to eq <<~SQUIGGLY_HEREDOC
       _
      |_|
       _|
