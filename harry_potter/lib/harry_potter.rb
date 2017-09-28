14fb11fa0b2c8295e7293e2648982a554a2a56c9
Dan Brooks <dmbrooks@live.co.uk>
2017-09-28 14:14:46 +0100

    Remove logic forking

--- harry_potter/lib/harry_potter.rb
+++ harry_potter/lib/harry_potter.rb
@@ -6,16 +6,12 @@ class Shop
     books = books.reject { |n| n == 0 }
     return 0 if books.empty?
 
-    if books.size == 5
-       books1 = books.map { |n| n - 1 }
-       cost1 = cost(books.size) + buy(books1)
-       books2 = books.take(4).map { |n| n - 1 }.concat(books.drop(4))
-       cost2 = cost(books.size-1) + buy(books2)
-       [cost1,cost2].min
-    else
-      books = books.map { |n| n - 1 }
-      cost(books.size) + buy(books)
-    end
+    less = [books.size - 1, 1].max
+    books1 = books.map { |n| n - 1 }
+    cost1 = cost(books.size) + buy(books1)
+    books2 = books.take(less).map { |n| n - 1 }.concat(books.drop(less))
+    cost2 = cost(less) + buy(books2)
+    [cost1,cost2].min
   end
 
   def discount(number)
